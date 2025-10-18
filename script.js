3443bad407dba0b6e0f73277bdf85d9f547d84bc
Mavis Aniewu <mharmhieyhaa1@gmail.com>
2025-10-18 14:38:30 +0000

    new updates

---
+++ script.js
@@ -0,0 +1,9 @@
+console.log('script loaded');
+
+
+const quoteDisplay = document.getElementById('quoteDisplay');
+const newQuoteBtn = document.getElementById('newQuote');
+const addQuoteBtn = document.getElementById('addQuoteBtn');
+
+// Temporary: show a placeholder text until we add the full logic
+quoteDisplay.textContent = "Click 'Show New Quote' to see a quote.";
